9c1bdd871a80e786f0a8c1a2cb2255b91c264ea7
dansarpong <dansarpong.me@gmail.com>
2024-05-19 20:37:33 +0000

    Task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+A simple Flask web application
+'''
+from flask import Flask
+
+
+'''The Flask application instance'''
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    '''The home page'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''The hbnb page'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def cisfun(text):
+    '''The C page'''
+    return 'C ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
